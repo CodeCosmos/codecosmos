f57a22c872942fd3db284c28a3a66a429fecfeb4
Bob Ippolito <bob@redivi.com>
2013-07-05 11:52:38 -0700

    appcache

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,39 @@
 module.exports = function (grunt) {
   'use strict';
+  var os = require('os');
+  var fs = require('fs');
+  var path = require('path');
+      
+  function generateAppCache(options) {
+    var prefix = 'dist';
+    var lines = [fs.readFileSync('www/codecosmos.appcache', 'UTF-8'),
+                 '',
+                 ['#', '[' + os.hostname() + ']', new Date()].join(' '),
+                 'CACHE:'];
+    function skip(fn) {
+      return (/^(\..*|URL|CNAME|VERSION|COPYING|Makefile|(bower|component|composer)\.json|demo\.html|diff_match_patch_test\..*|.*\.(appcache|txt|map)|LICENSE.*|README.*)$/.test(fn));
+    }
+    function walk(outputPrefix, dirname) {
+      fs.readdirSync(dirname).reduce(function (acc, fn) {
+        if (!skip(fn)) {
+          var inPath = path.join(dirname, fn);
+          var outPath = path.join(outputPrefix, fn);
+          var stats = fs.statSync(inPath);
+          if (stats.isFile()) {
+            lines.push(outPath);
+          } else if (stats.isDirectory()) {
+            // recurse later so all files come before directories
+            acc.push([outPath, inPath]);
+          }
+        }
+        return acc;
+      }, []).forEach(function (args) { walk.apply(null, args); });
+    }
+    walk('', 'dist');
+    lines.push('');
+    fs.writeFileSync('dist/codecosmos.appcache', lines.join('\n'));
+  }
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
@@ -26,6 +60,11 @@ module.exports = function (grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
+    },
+    copy: {
+      main: {
+        files: [{expand: true, src: ['**'], cwd: 'www/', dest: 'dist/'}]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -34,6 +73,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['test', 'build']);
-  grunt.registerTask('build', []);
+  grunt.registerTask('build', ['copy', 'appcache']);
   grunt.registerTask('test', ['jshint', 'csslint']);
+  grunt.registerTask('appcache', generateAppCache);
 };
