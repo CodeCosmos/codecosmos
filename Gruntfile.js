2ba661f5ce866c419657c05cc15726b7820f1207
Bob Ippolito <bob@redivi.com>
2013-07-05 11:04:00 -0700

    extract css and start linting it

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,24 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['gruntfile.js', 'www/js/*.js'],
+      files: ['Gruntfile.js', 'www/js/*.js'],
       options: {
         jshintrc: '.jshintrc'
+      }
+    },
+    csslint: {
+      lax: {
+        options: {
+          ids: false,
+          'overqualified-elements': false,
+          'compatible-vendor-prefixes': false,
+          'fallback-colors': false,
+          'box-sizing': false,
+          'box-model': false,
+          'qualified-headings': false,
+          'unique-headings': false
+        },
+        src: ['www/css/*.css']
       }
     },
     watch: {
@@ -13,9 +28,12 @@ module.exports = function (grunt) {
       tasks: ['jshint']
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('build', ['jshint']);
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['test', 'build']);
+  grunt.registerTask('build', []);
+  grunt.registerTask('test', ['jshint', 'csslint']);
 };
