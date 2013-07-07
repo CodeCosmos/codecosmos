3f778caa4fa4c40f823a289c80ed7e5518047e03
Bob Ippolito <bob@redivi.com>
2013-07-07 09:18:52 -0700

    refactor

--- www/data/formandcode-01.js
+++ www/data/formandcode-01.js
@@ -1,37 +1,31 @@
 var option = 1;
-processing.draw = function () {
-  processing.noFill();
-  processing.background(255);
+p.draw = function () {
   var x;
   var y;
-  var width = processing.width;
-  var height = processing.height;
-  var line = processing.line;
-  var ellipse = processing.ellipse;
-  var map = processing.map;
-  var TWO_PI = processing.TWO_PI;
-  var PI = processing.PI;
-  var arc = processing.arc;
+  var width = p.width;
+  var height = p.height;
+  p.noFill();
+  p.background(255);
   if (option === 1) {
     // Option 1: Stitches
     for (x = 50; x <= width - 50; x += 20) {
       for (y = 50; y <= height - 50; y += 20) {
-        line(x - 5, y - 5, x + 5, y + 5);
-        line(x + 5, y - 5, x - 5, y + 5);
+        p.line(x - 5, y - 5, x + 5, y + 5);
+        p.line(x + 5, y - 5, x - 5, y + 5);
       }
     }
   } else if (option === 2) {
     // Option 2: Perspective
     for (x = 50; x <= width - 50; x += 20) {
       for (y = 50; y <= height - 50; y += 20) {
-        line(x, y, width/2, height/2);
+        p.line(x, y, width/2, height/2);
       }
     }
   } else if (option === 3) {
     // Option 3: Overlapping circles
     for (x = 50; x <= width - 50; x += 20) {
       for (y = 50; y <= height - 50; y += 20) {
-        ellipse(x, y, 40, 40);
+        p.ellipse(x, y, 40, 40);
       }
     }
   } else if (option === 4) {
@@ -39,8 +33,8 @@ processing.draw = function () {
     var count = 120;
     for (x = 50; x <= width - 50; x += 20) {
       for (y = 50; y <= height - 50; y += 20) {
-        var s = map(count, 120, 0, 0, TWO_PI * 2);
-        arc(x, y, 14, 14, s, s + PI);
+        var s = p.map(count, 120, 0, 0, p.TWO_PI * 2);
+        p.arc(x, y, 14, 14, s, s + p.PI);
         count--;
       }
     }
@@ -49,17 +43,17 @@ processing.draw = function () {
     for (x = 50; x < width - 50; x += 20) {
       for (y = 50; y < height - 50; y += 20) {
         for (var i = 0; i < 16; i += 4) {
-          line(x + i, y, x + i, y + 12);
+          p.line(x + i, y, x + i, y + 12);
         }
-        line(x, y, x + 12 ,y + 12);
+        p.line(x, y, x + 12 ,y + 12);
       }
     }
   }
 };
-processing.mousePressed = function () {
+p.mousePressed = function () {
   option++;
   if (option > 5) {
     option = 1;
   }
-  processing.redraw();
+  p.redraw();
 };
